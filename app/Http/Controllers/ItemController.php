1f92e01098c5ca1a54989eb2dcba55efb6991a65
Haruto Ino <a18247h@reitaku.jp>
2021-05-16 21:40:22 +0900

    error-403-edit-ite-7

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -108,7 +108,7 @@ class ItemController extends Controller
      */
     public function edit($item)
     {
-        dd($item);
+        $item = (int)$item;
 
         $item = Item::where('id', $item)->with('user')->first();       
         $itemUserId = optional($item)->user_id;
@@ -130,6 +130,8 @@ class ItemController extends Controller
      */
     public function update(ItemRequest $request, $item)
     {   
+
+        $item = (int)$item;
         // 画像アップデート：画像を消す→新規追加
         $item = Item::where('id', $item)->with('user')->first();
         $itemUserId = optional($item)->user_id;
