e78cf1498e9fc0c7525a15b55b1145288faccc9e
Haruto Ino <a18247h@reitaku.jp>
2021-06-25 02:31:22 +0900

    fix-store-edit-show

--- resources/views/stores/edit.blade.php
+++ resources/views/stores/edit.blade.php
@@ -1,52 +1,72 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>store edit</h1>
-{{$store->store_owner_id}}
+    @if(count($errors)>0)
 
-@if(count($errors)>0)
+        @error('name')
+            <p>{{$message}}</p>
+        @enderror
 
-    @error('name')
-        <p>{{$message}}</p>
-    @enderror
+        @error('adress')
+            <p>{{$message}}</p>
+        @enderror
 
-    @error('adress')
-        <p>{{$message}}</p>
-    @enderror
+        @error('description')
+            <p>{{$message}}</p>
+        @enderror
 
-    @error('description')
-        <p>{{$message}}</p>
-    @enderror
+        @error('imgae')
+            <p>{{$message}}</p>
+        @enderror
 
-    @error('imgae')
-        <p>{{$message}}</p>
-    @enderror
+        @error('available')
+            <p>{{$message}}</p>
+        @enderror
 
-    @error('available')
-        <p>{{$message}}</p>
-    @enderror
+        @error('store_url')
+            <p>{{$message}}</p>
+        @enderror
 
-    @error('store_url')
-        <p>{{$message}}</p>
-    @enderror
+    @endif
 
-@endif
-
-<form method="POST" action="{{route('stores.update', ['store_owner' => Auth::id(), 'store' => $store])}}" enctype="multipart/form-data">
-    @method('PATCH')
-    @csrf        
-
-    <input name="name" type="text" placeholder="name" value="{{ old('name', $store->name) }}">
-    <input name="adress" type="text" placeholder="adress" value="{{ old('value', $store->adress) }}">
-    <input name="available" type="text" placeholder="available" value="{{ old('available', $store->available) }}">
-    <input name="store_url" type="text" placeholder="store_url" value="{{ old('store_url', $store->store_url) }}">        
-
-    <textarea name="description" id="" cols="30" rows="10" placeholder="description">{{old('description', $store->description)}}</textarea>
-    
-    <input type="submit" name="" id="" value="更新">
-</form>
+    <div id="item" class="container-fluid d-flex justify-content-center">
+        <div role="card" class="col-md-6 p-0 item-card m-5">        
+            @if(isset($store->image_path))
+            <img src="{{ $store->image_path }}" class="img img-responsive">
+            @else
+            <img src="https://res.cloudinary.com/delvmfnei/image/upload/v1621186998/1_eihryo.jpg" class="img img-responsive">
+            @endif
+            <div class="item-card-name">{{$store->name}}</div>
+            {{-- <div class="item-card-icons"><a href="#"><i class="fab fa-facebook"></i></a><a href="#"><i class="fab fa-twitter"></i></a><a href="#"><i class="fab fa-linkedin"></i></a></div> --}}        
+        </div>
+    </div>
 
 
+    <div class="container-fluid pb-5 d-flex justify-content-center">
+        <form class="col-md-6 d-flex flex-column" method="POST" action="{{route('stores.update', ['store_owner' => Auth::guard('store_owner')->id(), 'store' => $store] )}}" enctype="multipart/form-data">
+            @method('PATCH')
+            @csrf
+            <label class="d-flex flex-column" for="image">image
+                {{-- <input class="" name="title" type="file" placeholder="title"> --}}
+            </label>
+            <label class="d-flex flex-column" for="name">name
+                <input class="" name="name" type="text" placeholder="name" value="{{ old('name', $store->name) }}">
+            </label>
+            <label class="d-flex flex-column" for="adress">adress
+                <input class="" name="adress" type="text" placeholder="adress" value="{{ old('adress', $store->adress) }}">
+            </label>
+            <label class="d-flex flex-column" for="available">available
+                <input class="" name="available" type="text" placeholder="available" value="{{ old('available', $store->available) }}">
+            </label>
+            <label class="d-flex flex-column" for="store_url">store_url
+                <input class="" name="store_url" type="text" placeholder="store_url" value="{{ old('store_url', $store->store_url) }}">
+            </label>
+            <label class="d-flex flex-column" for="description">description
+                <textarea class="" name="description" id="" placeholder="description">{{old('description', $store->description)}}</textarea>
+            </label>
+            <input class="fas sumbmitInput" type="submit" name="" id="" value="&#xf2ea">
+        </form>
+    </div>
 
 @endsection
 
--- resources/views/stores/show.blade.php
+++ resources/views/stores/show.blade.php
@@ -1,16 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>store show</h1>
 
-<a href="{{ route('stores.edit', ['store_owner' => Auth::id(), 'store' => $store]) }}">編集する</a>
-<h1>{{$store->name}}</h1>
-
-<form action="{{ route('stores.destroy', ['store_owner' => Auth::id(), 'store' => $store]) }}" method="POST">
-    @method('DELETE')
-    @csrf
-    <input type="submit" value="削除">
-  </form>
-
-
+{{-- img --}}
+<div class="contaienr-fluid py-5">
+  <div class="col-7 col-sm-5 mx-auto">
+      <img class="card show-img" alt="" src="{{ $store->image_path }}">
+  </div>
+</div>
+{{-- card --}}
+<div class="contaienr-fluid mx-auto col-sm-10 pb-5">
+  <div class="card">
+      <div class="card-body bg-second">       
+          <div class="contaienr p-5">
+              <h5 class="text-title text-capitalize pb-3">{{$store->title}}</h5>          
+              <p class="text-text text-truncate">紹介：{{$store->description}}</p>
+              <p class="text-text text-truncate">空き状況：{{$store->available}}</p>
+              <hr class="bg-point">
+              <div class="container d-flex justify-content-around pt-5">
+                  {{-- <a href="{{$item->url}}" class="w-25 btn bg-main">ネットショップへ遷移する</a>    --}}
+                  <div class="d-flex align-items-center">
+                    <a class="" href="{{ route('stores.index', ['store_owner' => Auth::guard('store_owner')->id()] ) }}"><i class="fas fa-2x fa-user-edit"></i></a>  
+                  </div>
+                  <div class="d-flex align-items-center">
+                    <a class="" href="{{ route('stores.edit', ['store_owner' => Auth::guard('store_owner')->id(), 'store' => $store->id]) }}"><i class="fas fa-2x fa-edit"></i></a>
+                  </div>
+                  <div class="d-flex align-items-center">
+                    <form
+                      action="{{ route('stores.destroy', ['store_owner' => Auth::guard('store_owner')->id(), 'store' => $store] ) }}"
+                      method="POST"
+                    >
+                      @method('DELETE') @csrf
+                      <input class="fas fa-2x btn btn-main" type="submit" value="&#xf2ed">
+                    </form>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
 @endsection
